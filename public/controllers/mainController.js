b85590b9d711bc64f7455abbf99204194fd42158
Huy Tran <hqtran@wpi.edu>
2015-12-08 21:11:28 -0500

    initialize main view

--- public/controllers/mainController.js
+++ public/controllers/mainController.js
@@ -1,4 +1,17 @@
+/**
+ * mainController.js
+ * GET request to /colors
+ * Expected returning data: {color1, color2, ...}
+ */
 angular.module('WheelApp')
-  .controller('mainController', function ($scope) {
-    $scope.message = "Hello";
+  .controller('mainController', function ($scope, $http) {
+    $scope.listColors = ["red", "blue", "yellow"];
+    //$http({
+    //  method: 'GET',
+    //  url: '/colors'
+    //}).then(function successCallback(response) {
+    //  $scope.listColors = response;
+    //}, function errorCallback() {
+    //  console.log ('some errors happened');
+    //});
   });
