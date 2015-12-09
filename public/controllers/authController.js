015049afd24205c796a6b58a844f0c3cd2bf33a5
Huy Tran <hqtran@wpi.edu>
2015-12-08 20:33:51 -0500

    add ui.route

---
+++ public/controllers/authController.js
@@ -0,0 +1,34 @@
+angular.module('WheelApp')
+  .controller('authController', function ($scope, $http, $rootScope, $location) {
+    $scope.user = {username: "", password: ""};
+    $scope.error_message = '';
+    $scope.colors = [];
+
+    $scope.login = function() {
+      console.log('Hello world');
+      // AJAX CALL BACK
+      $http.post(
+        // TODO: POST request to 'auth/login'
+        // POST DATA TYPE EXAMPLE
+        // JSON DATA
+        // "{username: "abc", password: "123"}"
+        '/auth/login',
+        JSON.stringify($scope.user)
+      ).then(
+        // TODO: If authentication is valid, please respond with user info
+        // Username, colors chosen history, points ... Nam's decision
+        // Require data: id, username, color, state (success, fail)
+        function success (response) {
+          if (response.state = 'success') {
+            $rootScope.authenticated = true;
+            $rootScope.currentUser = response.username;
+
+            $location.path('/');
+          }
+        },
+        function error (response) {
+          $scope.error_message = response;
+        }
+      );
+    }
+  });
--- public/wheelApp.js
+++ public/wheelApp.js
@@ -1,20 +1,38 @@
-angular.module('WheelApp', ['ngRoute', 'ngResource'])
-  .config(function ($routeProvider) {
-  $routeProvider
-    .when('/', {
+angular.module('WheelApp', ['ngRoute', 'ngResource', 'ui.router'])
+  // Initialization and log out
+  .run(function ($rootScope, $http) {
+    // No user logged in
+    $rootScope.authenticated = false;
+    $rootScope.currentUser = "";
+    // reset state of the app
+    $rootScope.signout = function(){
+      $http.get('auth/signout');
+      $rootScope.authenticated = false;
+      $rootScope.currentUser= '';
+    };
+  })
+  // Routing Configuration
+  .config([function ($stateProvider) {
+  $stateProvider
+    .state('main', {
+      url: '/',
       templateUrl: 'views/main.html',
       controller: 'mainController'
     })
-    .when('/login', {
+    .state('login', {
+      url: '/login',
       templateUrl: 'views/login.html',
       controller: 'authController'
     })
-    .when('/signup', {
+    .state('signup', {
+      url: '/signup',
       templateUrl: 'views/signup.html',
       controller: 'authController'
     })
-    .when('/about', {
+    .state('about', {
+      url: '/about',
       templateUrl: 'views/about.html',
       controller: 'aboutController'
     })
-});
+  }]);
+
