614b694559a778d97cfc7ec90465d935f155f72c
Huy Tran <hqtran@wpi.edu>
2015-12-08 21:57:09 -0500

    uncomment to test mock functionality

--- public/controllers/authController.js
+++ public/controllers/authController.js
@@ -2,41 +2,41 @@ angular.module('WheelApp')
   .controller('authController', function ($scope, $http, $rootScope, $location) {
     $scope.user = {username: "", password: ""};
     $scope.error_message = '';
-    $scope.colors = [];
 
-    //$scope.login = function() {
-    //  console.log('Hello world');
-    //  // AJAX CALL BACK
+    //function authenticate(url) {
     //  $http.post(
-    //    // TODO: POST request to 'auth/login'
-    //    // POST DATA TYPE EXAMPLE
-    //    // JSON DATA
-    //    // "{username: "abc", password: "123"}"
-    //    '/auth/login',
+    //    url,
     //    JSON.stringify($scope.user)
-    //  ).then(
-    //    // TODO: If authentication is valid, please respond with user info
-    //    // Username, colors chosen history, points ... Nam's decision
-    //    // Require data: id, username, color, state (success, fail)
-    //    function success (response) {
-    //      if (response.state === 'success') {
+    //  ).success(function putDataInScope(response) {
+    //      var data = JSON.parse(response);
+    //      if (data.state === "success") {
     //        $rootScope.authenticated = true;
     //        $rootScope.currentUser = response.username;
     //
     //        $location.path('/user');
     //      }
-    //    },
-    //    function error (response) {
-    //      $scope.error_message = response;
+    //      else if (data.state === "error") {
+    //        $scope.error_message = data.error_message;
+    //      }
     //    }
-    //  );
+    //  )
     //}
+
+    //$scope.login = function () {
+    //  authenticate('/auth/login')
+    //};
+    //$scope.signup = function () {
+    //  authenticate('/auth/signup')
+    //};
 
     // MOCK LOGIN
     $scope.login = function () {
       // success
-      $location.path('/user');
-      $rootScope.currentUser = "Huy Tran";
-      $rootScope.authenticated = true;
+      //$location.path('/user');
+      //$rootScope.currentUser = "Huy Tran";
+      //$rootScope.authenticated = true;
+
+      // fail
+      $scope.error_message = "Invalid user or password";
     }
   });
