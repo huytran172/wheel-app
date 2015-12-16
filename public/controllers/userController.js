e8a49b3709bc8ed17bc80bd1699b8e236a26bb78
Huy Tran <hqtran@wpi.edu>
2015-12-16 14:52:50 -0500

    start implement user page

--- public/controllers/userController.js
+++ public/controllers/userController.js
@@ -1,13 +1,5 @@
 angular.module('WheelApp')
   .controller('userController', function ($location, $scope, $rootScope, $resource) {
     if ($rootScope.authenticated) {
-      // Get all question answered correctly by that user
-      var Question = $resource('/questions/:userId', {userId:'@id'});
-      var questions = Question.query({userId:123}, function() {
-        $scope.questionsList = questions;
-      });
-    }
-    else {
-      $location.url('/');
-    }
-  });
+      
+    });
