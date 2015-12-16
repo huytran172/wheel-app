c186720f5c76430449b88269aefc8f89164460ee
Huy Tran <hqtran@wpi.edu>
2015-12-16 14:54:30 -0500

    start implement user page

--- public/controllers/userController.js
+++ public/controllers/userController.js
@@ -1,5 +1,22 @@
 angular.module('WheelApp')
-  .controller('userController', function ($location, $scope, $rootScope, $resource) {
+  .controller('userController', function ($location, $scope, $rootScope, $http) {
     if ($rootScope.authenticated) {
-      
-    });
+      $http.get('/questions/question/' + $rootScope.currentUser._id).success(function (response) {
+        $scope.questionsList = response;
+        $scope.mostRecentWin = 0;
+        response.forEach(function (item) {
+          if (item.answerAt > $scope.mostRecentWin) {
+            $scope.mostRecentWin = item.answerAt;
+          }
+        });
+      });
+      $http.post('/questions/userRecord', {_id: $rootScope.currentUser._id}).success(function (response) {
+        console.log(response);
+        $scope.points = response.points;
+      })
+
+    }
+    else {
+      $location.url('/');
+    }
+  });
